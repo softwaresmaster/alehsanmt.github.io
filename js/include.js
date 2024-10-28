4fb7c05934c8cc537f0f6b88e50798ae9944a2e4
Syed Ehsan Ul Haq <softwaresmasters70@gmail.com>
2024-10-28 12:34:33 +0530

    appliance repair page completed

--- js/include.js
+++ js/include.js
@@ -7,7 +7,15 @@ $("#contact").load("include/contact.html");
 // footer
 $("#footer").load("include/footer.html");
 
-// header wall arts
+
+
+// Wall arts
 $("#header2").load("include/header2.html");
+
+// Appliance Repair
+// footer in appliance
+$("#footer3").load("include/footer3.html");
 // header appliance repair
 $("#header3").load("include/header3.html");
+// contact form in appliance 
+$("#contact3").load("include/contact3.html");
